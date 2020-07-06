9ec2cd39d9020867be956c1cfc764e1a1d80b37a
“Zach <z_wood@baylor.edu>
2020-07-06 00:39:27 +0000

    finished grammable from backed up git file after deletion error

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "grams#index"
+  # resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :grams do
+    resources :comments, only: :create
+  end
+end  
---
+++ spec/factories.rb
@@ -0,0 +1,19 @@
+# FactoryBot.define do
+#   factory :comment do
+
+#   end
+#   factory :user do
+#     sequence :email do |n|
+#       "dummyEmail#{n}@gmail.com"
+#     end
+#     password "secretPassword"
+#     password_confirmation "secretPassword"
+#   end
+
+#   factory :gram do
+#     message "hello"
+#     picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
+
+#     association :user
+#   end
+# end
